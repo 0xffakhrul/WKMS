02eac2ff17974ec643d08607d4100a12e73935a1
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2023-12-29 21:12:18 +0800

    migrations

---
+++ database/migrations/2023_12_29_011557_create_attendances_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->datetime('check_in_time')->default(now());
+            $table->datetime('check_out_time')->nullable(); // Allow NULL for check_out_time
+            $table->date('date')->default(now()->toDateString());
+            $table->string('status');
+            $table->timestamps();
+
+            // Define the foreign key constraint
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
