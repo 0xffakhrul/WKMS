6efe91540aae8f0e37a57b597a9a488b26582b6c
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2023-12-29 21:02:32 +0800

    display today's attendance in table

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Attendance;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function AdminDashboard()
     {
-        return view('admin.dashboard');
+        // Retrieve all users
+        $users = User::all();
+
+        // Retrieve today's attendance records for all users
+        $todayAttendances = Attendance::whereDate('date', Carbon::today())->get();
+
+        return view('admin.dashboard', compact('users', 'todayAttendances'));
     }
 }
