f620dbd168e7b6895fb4509682f6f4f3e478208c
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-01-02 09:12:37 +0800

    leave index ui fix

--- resources/views/admin/leaves/index.blade.php
+++ resources/views/admin/leaves/index.blade.php
@@ -1,22 +1,29 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col gap-y-6">
-            <div class="flex justify-between">
-                <div class=" text-gray-900 text-3xl font-bold">
-                    {{ __('Employees List') }}
+            <div class="flex justify-between items-center pt-10">
+                <div class=" text-gray-900 font-bold text-2xl">
+                    {{ __('Leave Requests List') }}
                 </div>
                 <button class="px-4 py-2 rounded-lg font-bold bg-green-600 text-white"><a
-                        href="{{ route('admin.employees.create') }}">New Employee</a> </button>
+                        href="{{ route('admin.leaves.create') }}">New Leave Requests</a> </button>
             </div>
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <div class="relative overflow-x-auto rounded-lg bg-white">
+                <label for="table-search" class="sr-only">Search</label>
+                <div class="relative m-4">
+                    <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-950" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
+                    </div>
+                    <input type="text" id="table-search"
+                        class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-60 bg-white focus:ring-blue-500 focus:border-blue-500 "
+                        placeholder="Search">
+                </div>
                 <table class="w-full text-sm text-left rtl:text-right text-gray-950">
-                    <thead class="text-xs text-gray-950 uppercase bg-gray-200">
+                    <thead class="text-xs text-gray-950 uppercase bg-gray-50 border-b border-t border-gray-200">
                         <tr>
                             <th class="px-6 py-3.5">No.</th>
                             <th class="px-6 py-3.5">Employee</th>
@@ -40,38 +47,58 @@
                                 </td>
                                 <td class="px-6 py-4">{{ $leave->start_date }}</td>
                                 <td class="px-6 py-4">{{ $leave->end_date }}</td>
-                                <td class="px-6 py-4">{{ $leave->leave_type }}</td>
-                                <td class="px-6 py-4">{{ $leave->status }}</td>
+                                <td class="px-6 py-4">
+                                    @if ($leave->leave_type === 'annual')
+                                        Annual
+                                    @else
+                                        Sick
+                                    @endif
+                                </td>
+                                <td class="px-6 py-4">
+                                    @if ($leave->status === 'approved')
+                                        <span
+                                            class="bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded border border-green-400">Approved</span>
+                                    @elseif ($leave->status === 'rejected')
+                                        <span
+                                            class="bg-red-100 text-red-800 text-xs font-medium px-2.5 py-0.5 rounded border border-red-400">Rejected</span>
+                                    @else
+                                        <span
+                                            class="bg-yellow-100 text-yellow-800 text-xs font-medium px-2.5 py-0.5 rounded border border-yellow-400">Pending</span>
+                                    @endif
+                                </td>
 
-                                <td class="px-6 py-4 flex gap-x-3">
-                                    {{-- <a href="{{ route('admin.leave.edit', $leave->id) }}"
+                                <td class="px-6 py-4 flex gap-x-3 items-center">
+                                    <a href="{{ route('admin.leaves.edit', $leave->id) }}"
                                         class="text-blue-600 hover:underline inline-flex items-center justify-center outline-none gap-1">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                             stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                            <!-- Edit Icon SVG Path -->
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                         </svg>
                                         Edit
-                                    </a> --}}
-                                    {{-- <form action="{{ route('admin.leave.destroy', $leave->id) }}" method="POST"
+                                    </a>
+                                    <form action="{{ route('admin.leaves.destroy', $leave->id) }}" method="POST"
                                         class="text-red-600 hover:underline inline-flex items-center justify-center outline-none gap-1">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit"
-                                            class="text-red-600 hover:underline inline-flex items-center justify-center outline-none gap-1">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            class="text-red-600 hover:underline inline-flex items-center justify-center outline-none gap-1"><svg
+                                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                 stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                                <!-- Delete Icon SVG Path -->
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                             </svg>
-                                            Delete
-                                        </button>
-                                    </form> --}}
+                                            Delete</button>
+                                    </form>
                                 </td>
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
+                <div class="py-4 px-6">
+                    {{ $leaves->links() }}
+                </div>
             </div>
         </div>
-
     </div>
 </x-app-layout>
