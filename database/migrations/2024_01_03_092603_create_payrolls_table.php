b29507d50c5d0b1a6c8e093e717b281f43a1513e
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-01-07 22:06:16 +0800

    adding migrations

---
+++ database/migrations/2024_01_03_092603_create_payrolls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payrolls', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->decimal('salary', 10, 2);
+            $table->datetime('pay_date')->default(now());
+            $table->string('status');
+            $table->timestamps();
+
+            // Define the foreign key constraint
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payrolls');
+    }
+};
